82a53d052286babadded8ecb5fd8cc4cff37fa90
Mathias Bigaignon <m.bigaignon@xefi.fr>
2024-07-02 17:26:07 +0200

    Some config updates

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Python Entitled',
+    description: 'Authorization made easy.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
